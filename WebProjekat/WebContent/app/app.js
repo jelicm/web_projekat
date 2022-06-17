8156177145322c3b1958513f881f5e5f7b322361
Milena Jelic <jelic.ra23.2019@uns.ac.rs>
2022-06-17 21:21:31 +0200

    Sport Facility search

--- WebProjekat/WebContent/app/app.js
+++ WebProjekat/WebContent/app/app.js
@@ -3,13 +3,21 @@ var app = new Vue({
     data:{
         korisnickoIme: '',
 		lozinka: '',
-		objekti: {}
+		objekti: {},
+		pretrazeniObjekti: {},
+		naziv: '',
+		tip: '',
+		lokacija: '',
+		prosecnaOcena: ''
     },
 
 	mounted() {
 		axios
 			.get("rest/sportFacilities")
-			.then((response) => (this.objekti = response.data));
+			.then((response) => {
+				this.objekti = response.data;
+				this.pretrazeniObjekti = response.data;	
+			});
 	},
 	
     methods: {
@@ -26,6 +34,40 @@ var app = new Vue({
             .catch(function(){
                 alert("Neispravni kredencijali!")
             })
-        }
+        },
+		pretragaNaziv: function(naziv){
+			this.pretrazeniObjekti = this.objekti.filter(o => o.name.toLowerCase().includes(naziv.toLowerCase()));
+			
+			this.prosecnaOcena = '';
+			this.tip = '';
+			this.lokacija = '';
+		
+		},
+		pretragaLokacija: function(lokacija){
+			this.pretrazeniObjekti = this.objekti.filter(o => (o.location.address.street.toLowerCase().includes(lokacija.toLowerCase()) || o.location.address.city.toLowerCase().includes(lokacija.toLowerCase())));
+			this.naziv = '';
+			this.prosecnaOcena = '';
+			this.tip = '';
+			
+		},
+		pretragaOcena: function(prosecnaOcena){
+			this.pretrazeniObjekti = this.objekti.filter(o => o.averageRating == prosecnaOcena);
+			this.naziv = '';
+			this.tip = '';
+			this.lokacija = '';
+		},
+		pretragaTip: function(tip){
+			this.pretrazeniObjekti = this.objekti.filter(o => o.type.toLowerCase().replace('_', '').includes(tip.replace(' ','').toLowerCase()));
+			this.naziv = '';
+			this.prosecnaOcena = '';
+			this.lokacija = '';
+		},
+		ponistiPretragu: function(){
+			this.pretrazeniObjekti = this.objekti;
+			this.naziv = '';
+			this.prosecnaOcena = '';
+			this.tip = '';
+			this.lokacija = '';
+		}
     }
 })

e1359c42e5dbb2c34a34bb1d6f85457862170a50
Milena Jelic <jelic.ra23.2019@uns.ac.rs>
2022-07-08 22:04:58 +0200

    Sport facility location on map

--- WebProjekat/WebContent/sportFacilityInfo.js
+++ WebProjekat/WebContent/sportFacilityInfo.js
@@ -13,7 +13,41 @@ var app = new Vue({
 			.then(response => {this.treninzi = response.data})
 			axios.get('rest/comments/approvedCommentsForFacility/' + this.sportFacility.name)
 			.then(response => {this.komentari = response.data})
-		})
-		
+			
+			var iconFeature = new ol.Feature({
+		  	geometry: new ol.geom.Point(ol.proj.fromLonLat([this.sportFacility.location.longitude, this.sportFacility.location.latitude])),
+		  	name: this.sportFacility.name,
+			});
+	 		var map = new ol.Map({
+		         target: 'map',
+		         layers: [
+		           new ol.layer.Tile({
+		             source: new ol.source.OSM()
+		           }),
+		           new ol.layer.Vector({
+		        	      source: new ol.source.Vector({
+		        	        features: [iconFeature]
+		        	      }),
+		        	      style: new ol.style.Style({
+		        	        image: new ol.style.Icon({
+		        	          anchor: [0.5, 46],
+		        	          anchorXUnits: 'fraction',
+		        	          anchorYUnits: 'pixels',
+		        	          src: 'img/marker.png' 
+		        	        })
+	        	      })
+        	    })
+        	  
+         ],
+         
+         view: new ol.View({
+           center: ol.proj.fromLonLat([this.sportFacility.location.longitude, this.sportFacility.location.latitude]),
+           zoom: 4 
+         }),
+
+       });
+			
+	})
+	
 	}
 })
